c0630f944d31e3c2fe1f113a5973aed40e528cd7
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-09-16 07:10:28 -0400

    Initial Commit

---
+++ GenaricRealmTest/GenaricRealmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenaricRealmTestApp.swift
+//  GenaricRealmTest
+//
+//  Created by Ethan Kisiel on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GenaricRealmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
